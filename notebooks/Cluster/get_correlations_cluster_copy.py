46145b153c49c7977530e90898a42ab9bb092ac0
zagglo <leozaggia@gmail.com>
2023-08-14 13:25:05 +0200

    added different model for the estimation of the graph measures

--- notebooks/Cluster/get_correlations_cluster_copy.py
+++ notebooks/Cluster/get_correlations_cluster_copy.py
@@ -14,8 +14,13 @@ path = "/gss/work/head3827/root_dir/data/pipeline_timeseries"
 age_path = "/gss/work/head3827/root_dir/data/combined.tsv"
 output_path = "/gss/work/head3827/root_dir/outputs"
 Exhaustive_path = output_path + "/Exhaustive/" + key
+
 PredictAccs_linear_path = Exhaustive_path + "/PredictAccs_linear"
 PredictAccs_tree_path = Exhaustive_path + "/PredictAccs_tree"
+PredictAccs_Ridge_path = Exhaustive_path + "/PredictAccs_Ridge"
+PredictAccs_Lasso_path = Exhaustive_path + "/PredictAccs_Lasso"
+PredictAccs_ElasticNet_path = Exhaustive_path + "/PredictAccs_ElasticNet"
+
 Multi_path = Exhaustive_path + "/MultiversePipelines"
 path = "/gss/work/head3827/root_dir/data/pipeline_timeseries"
 age_path = "/gss/work/head3827/root_dir/data/combined.tsv"
@@ -29,6 +34,7 @@ data_space, data_predict, data_lockbox = split_data(data = data,
 
 
 i = (int(sys.argv[1])-1)
+n_ROIs = len(list(data_predict["ts"][0].keys()))
 age = AgesPrediction = np.asanyarray(data_predict["b_age"])
 TempResults = pickle.load(open(str(Exhaustive_path + "/" + 'exhaustive_search_results.p'), 'rb'))["199_subjects_1152_pipelines"]
 age_list = list(map(lambda x: [x], age))
@@ -39,14 +45,30 @@ X_train, X_test, y_train, y_test = train_test_split(age_list, tmp_list,
 
 model_linear = MultiOutputRegressor(LinearRegression())
 model_tree =  MultiOutputRegressor(RandomForestRegressor())
+model_Ridge = MultiOutputRegressor(Ridge())
+model_Lasso = MultiOutputRegressor(Lasso())
+model_ElasticNet = MultiOutputRegressor(ElasticNet())
+
 model_linear.fit(X_train, y_train)
 model_tree.fit(X_train, y_train)
+model_Ridge.fit(X_train, y_train)
+model_Lasso.fit(X_train, y_train)
+model_ElasticNet.fit(X_train, y_train)
 
 pred_linear = model_linear.predict(X_test)
 pred_tree = model_tree.predict(X_test)
+pred_Ridge = model_Ridge.predict(X_test)
+pred_Lasso = model_Lasso.predict(X_test)
+pred_ElasticNet = model_ElasticNet.predict(X_test)
 
-scores_tree = np.mean(np.diag(np.corrcoef(np.array(y_test), pred_tree)))
-scores_linear = np.mean(np.diag(np.corrcoef(np.array(y_test), pred_linear)))
+scores_tree = np.diagonal(np.corrcoef(pred_tree, np.array(y_test), rowvar=False)[:n_ROIs, n_ROIs:])
+scores_linear = np.diagonal(np.corrcoef(pred_linear, np.array(y_test), rowvar=False)[:n_ROIs, n_ROIs:])
+scores_Ridge = np.diagonal(np.corrcoef(pred_Ridge, np.array(y_test), rowvar=False)[:n_ROIs, n_ROIs:])
+scores_Lasso = np.diagonal(np.corrcoef(pred_Lasso, np.array(y_test), rowvar=False)[:n_ROIs, n_ROIs:])
+scores_ElasticNet = np.diagonal(np.corrcoef(pred_ElasticNet, np.array(y_test), rowvar=False)[:n_ROIs, n_ROIs:])
 
 pickle.dump(scores_linear, open(str(PredictAccs_linear_path + "/" + str(i) + '.p'), 'wb'))
 pickle.dump(scores_tree, open(str(PredictAccs_tree_path + "/" + str(i) + '.p'), 'wb'))
+pickle.dump(scores_Ridge, open(str(PredictAccs_Ridge_path + "/" + str(i) + '.p'), 'wb'))
+pickle.dump(scores_Lasso, open(str(PredictAccs_Lasso_path + "/" + str(i) + '.p'), 'wb'))
+pickle.dump(scores_ElasticNet, open(str(PredictAccs_ElasticNet_path + "/" + str(i) + '.p'), 'wb'))
--- notebooks/Cluster/patch_correlations_cluster_copy.py
+++ notebooks/Cluster/patch_correlations_cluster_copy.py
@@ -9,8 +9,13 @@ path = "/gss/work/head3827/root_dir/data/pipeline_timeseries"
 age_path = "/gss/work/head3827/root_dir/data/combined.tsv"
 output_path = "/gss/work/head3827/root_dir/outputs"
 Exhaustive_path = output_path + "/Exhaustive/" + key
+
 PredictAccs_linear_path = Exhaustive_path + "/PredictAccs_linear"
 PredictAccs_tree_path = Exhaustive_path + "/PredictAccs_tree"
+PredictAccs_Ridge_path = Exhaustive_path + "/PredictAccs_Ridge"
+PredictAccs_Lasso_path = Exhaustive_path + "/PredictAccs_Lasso"
+PredictAccs_ElasticNet_path = Exhaustive_path + "/PredictAccs_ElasticNet"
+
 Multi_path = Exhaustive_path + "/MultiversePipelines"
 
 def get_forest_acc():
@@ -27,11 +32,38 @@ def get_linear_acc():
         linear_acc.append(pickle.load(open(str(PredictAccs_linear_path + "/" + str(i) + ".p"), "rb")))
     pickle.dump(linear_acc, open(str(Exhaustive_path + "/" + 'linear_corr_acc.p'), 'wb'))     
 
+def get_Ridge_acc():
+    Ridge_acc = []
+    files = os.listdir(PredictAccs_linear_path)
+    for i, _ in enumerate(files):
+        Ridge_acc.append(pickle.load(open(str(PredictAccs_Ridge_path + "/" + str(i) + ".p"), "rb")))
+    pickle.dump(Ridge_acc, open(str(Exhaustive_path + "/" + 'Ridge_corr_acc.p'), 'wb'))
+
+def get_Lasso_acc():
+    Lasso_acc = []
+    files = os.listdir(PredictAccs_linear_path)
+    for i, _ in enumerate(files):
+        Lasso_acc.append(pickle.load(open(str(PredictAccs_Lasso_path + "/" + str(i) + ".p"), "rb")))
+    pickle.dump(Lasso_acc, open(str(Exhaustive_path + "/" + 'Lasso_corr_acc.p'), 'wb'))
+
+def get_ElasticNet_acc():
+    ElasticNet_acc = []
+    files = os.listdir(PredictAccs_linear_path)
+    for i, _ in enumerate(files):
+        ElasticNet_acc.append(pickle.load(open(str(PredictAccs_ElasticNet_path + "/" + str(i) + ".p"), "rb")))
+    pickle.dump(ElasticNet_acc, open(str(Exhaustive_path + "/" + 'ElasticNet_corr_acc.p'), 'wb'))
 
 t2 = Thread(target = get_linear_acc)
 t2.start()
 t3 = Thread(target = get_forest_acc)
 t3.start()
+t4 = Thread(target = get_Ridge_acc)
+t4.start()
+t5 = Thread(target = get_Lasso_acc)
+t5.start()
+t6 = Thread(target = get_ElasticNet_acc)
+t6.start()
+
 
 
 
